2cf3b1c9790ab5b827a47d3caed07574f3a21fce
Mohit Meena <139558066+Rajkholwal@users.noreply.github.com>
2024-11-05 02:56:20 +0530

    Optimised booleanAlgebraFinal.py

--- backend/Main/Boolean_Algebra/booleanAlgebraFinal.py
+++ backend/Main/Boolean_Algebra/booleanAlgebraFinal.py
@@ -295,10 +295,10 @@ def removeTerms(_chart,terms):
 
 def print_QM(mt, dc, char):
     mt.sort()
-    minterms = mt+dc
+    minterms = mt + dc
     minterms.sort()
-    size = len(bin(minterms[-1]))-2
-    groups,all_pi = {},set()
+    size = len(bin(minterms[-1])) - 2
+    groups, all_pi = {}, set()
 
     for minterm in minterms:
         try:
@@ -308,17 +308,17 @@ def print_QM(mt, dc, char):
 
     while True:
         tmp = groups.copy()
-        groups,m,marked,should_stop = {},0,set(),True
+        groups, m, marked, should_stop = {}, 0, set(), True
         l = sorted(list(tmp.keys()))
-        for i in range(len(l)-1):
+        for i in range(len(l) - 1):
             for j in tmp[l[i]]:
-                for k in tmp[l[i+1]]:
-                    res = compare(j,k)
+                for k in tmp[l[i + 1]]:
+                    res = compare(j, k)
                     if res[0]:
                         try:
-                            groups[m].append(j[:res[1]]+'-'+j[res[1]+1:]) if j[:res[1]]+'-'+j[res[1]+1:] not in groups[m] else None
+                            groups[m].append(j[:res[1]] + '-' + j[res[1] + 1:]) if j[:res[1]] + '-' + j[res[1] + 1:] not in groups[m] else None
                         except KeyError:
-                            groups[m] = [j[:res[1]]+'-'+j[res[1]+1:]]
+                            groups[m] = [j[:res[1]] + '-' + j[res[1] + 1:]]
                         should_stop = False
                         marked.add(j)
                         marked.add(k)
@@ -332,53 +332,53 @@ def print_QM(mt, dc, char):
     chart = {}
 
     for i in all_pi:
-        merged_minterms,y = findminterms(i),0
+        merged_minterms, y = findminterms(i), 0
 
-        for j in refine(merged_minterms,dc):
-            x = mt.index(int(j))*(sz+1)
-            y = x+sz
+        for j in refine(merged_minterms, dc):
+            x = mt.index(int(j)) * (sz + 1)
+            y = x + sz
             try:
                 chart[j].append(i) if i not in chart[j] else None
             except KeyError:
                 chart[j] = [i]
 
     EPI = findEPI(chart)
-    removeTerms(chart,EPI)
+    removeTerms(chart, EPI)
 
-    if(len(chart) == 0):
+    if len(chart) == 0:
         final_result = [findVariables(i, char) for i in EPI]
     else:
         P = [[findVariables(j, char) for j in chart[i]] for i in chart]
-        while len(P)>1:
-            P[1] = multiply(P[0],P[1])
+        while len(P) > 1:
+            P[1] = multiply(P[0], P[1])
             P.pop(0)
-        final_result = [min(P[0],key=len)]
+        final_result = [min(P[0], key=len)] if P[0] else []
         final_result.extend(findVariables(i, char) for i in EPI)
-    return "{0}".format('F = '+' + '.join(''.join(i) for i in final_result))
+    return "{0}".format('F = ' + ' + '.join(''.join(i) for i in final_result))
 
 
 def bit_flip_var(x, char):
     var_list = []
     for i in range(len(x)):
-        if(random.randint(0,1)==0):
+        if random.randint(0, 1) == 0:
             if x[i] == '0':
-                var_list.append(chr(i+ord(char)))
+                var_list.append(chr(i + ord(char)))
             elif x[i] == '1':
-                var_list.append(chr(i+ord(char))+"'")
+                var_list.append(chr(i + ord(char)) + "'")
         else:
             if x[i] == '0':
-                var_list.append(chr(i+ord(char))+"'")
+                var_list.append(chr(i + ord(char)) + "'")
             elif x[i] == '1':
-                var_list.append(chr(i+ord(char)))
+                var_list.append(chr(i + ord(char)))
     return var_list
 
 
 def print_QM_distract(mt, dc, char):
     mt.sort()
-    minterms = mt+dc
+    minterms = mt + dc
     minterms.sort()
-    size = len(bin(minterms[-1]))-2
-    groups,all_pi = {},set()
+    size = len(bin(minterms[-1])) - 2
+    groups, all_pi = {}, set()
 
     for minterm in minterms:
         try:
@@ -388,17 +388,17 @@ def print_QM_distract(mt, dc, char):
 
     while True:
         tmp = groups.copy()
-        groups,m,marked,should_stop = {},0,set(),True
+        groups, m, marked, should_stop = {}, 0, set(), True
         l = sorted(list(tmp.keys()))
-        for i in range(len(l)-1):
+        for i in range(len(l) - 1):
             for j in tmp[l[i]]:
-                for k in tmp[l[i+1]]:
-                    res = compare(j,k)
+                for k in tmp[l[i + 1]]:
+                    res = compare(j, k)
                     if res[0]:
                         try:
-                            groups[m].append(j[:res[1]]+'-'+j[res[1]+1:]) if j[:res[1]]+'-'+j[res[1]+1:] not in groups[m] else None
+                            groups[m].append(j[:res[1]] + '-' + j[res[1] + 1:]) if j[:res[1]] + '-' + j[res[1] + 1:] not in groups[m] else None
                         except KeyError:
-                            groups[m] = [j[:res[1]]+'-'+j[res[1]+1:]]
+                            groups[m] = [j[:res[1]] + '-' + j[res[1] + 1:]]
                         should_stop = False
                         marked.add(j)
                         marked.add(k)
@@ -412,45 +412,45 @@ def print_QM_distract(mt, dc, char):
     chart = {}
 
     for i in all_pi:
-        merged_minterms,y = findminterms(i),0
+        merged_minterms, y = findminterms(i), 0
 
-        for j in refine(merged_minterms,dc):
-            x = mt.index(int(j))*(sz+1)
-            y = x+sz
+        for j in refine(merged_minterms, dc):
+            x = mt.index(int(j)) * (sz + 1)
+            y = x + sz
             try:
                 chart[j].append(i) if i not in chart[j] else None
             except KeyError:
                 chart[j] = [i]
 
     EPI = findEPI(chart)
-    removeTerms(chart,EPI)
+    removeTerms(chart, EPI)
 
-    if(len(chart) == 0):
+    if len(chart) == 0:
         final_result = [bit_flip_var(i, char) for i in EPI]
     else:
         P = [[bit_flip_var(j, char) for j in chart[i]] for i in chart]
-        while len(P)>1:
-            P[1] = multiply(P[0],P[1])
+        while len(P) > 1:
+            P[1] = multiply(P[0], P[1])
             P.pop(0)
-        final_result = [min(P[0],key=len)]
+        final_result = [min(P[0], key=len)] if P[0] else []
         final_result.extend(bit_flip_var(i, char) for i in EPI)
-    return "{0}".format('F = '+' + '.join(''.join(i) for i in final_result))
+    return "{0}".format('F = ' + ' + '.join(''.join(i) for i in final_result))
 
 
 def manipulate_minterms(table):
     mt, num_vars = table_to_minterms(table)
     count = len(mt)
-    for i in range (len(table)):
-        if i not in mt and random.randint(0,6)<=1:
+    for i in range(len(table)):
+        if i not in mt and random.randint(0, 6) <= 1:
             mt.append(i)
-        if i in mt and random.randint(0,6)<=1:
-            if len(mt)>1:
+        if i in mt and random.randint(0, 6) <= 1:
+            if len(mt) > 1:
                 mt.remove(i)
-    if len(mt)<count:
+    if len(mt) < count:
         for i in range(len(table)):
             if i not in mt:
                 mt.append(i)
-                if len(mt)>=count:
+                if len(mt) >= count:
                     break
 
     random.shuffle(mt)
@@ -458,17 +458,17 @@ def manipulate_minterms(table):
 
 
 def manipulate_dc(table):
-    dc=[]
+    dc = []
     mt, num_vars = table_to_minterms(table)
     count = len(mt)
-    for i in range (len(table)):
-        if i not in mt and random.randint(0,6)<=1:
+    for i in range(len(table)):
+        if i not in mt and random.randint(0, 6) <= 1:
             dc.append(i)
-    if len(dc)<count:
+    if len(dc) < count:
         for i in range(len(table)):
             if i not in dc:
                 dc.append(i)
-                if len(dc)>=count:
+                if len(dc) >= count:
                     break
     random.shuffle(dc)
     return dc[0:count]
