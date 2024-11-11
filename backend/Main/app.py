2436465e9f0a12c81e729a5adfd3948a9cfa34ba
Mohit Meena <139558066+Rajkholwal@users.noreply.github.com>
2024-11-11 16:23:28 +0530

    Update app.py

--- backend/Main/app.py
+++ backend/Main/app.py
@@ -150,74 +150,7 @@
 #     app.run(debug=True)
 
 
-from flask import Flask, render_template, request, jsonify, send_from_directory, send_file, url_for, redirect
-from flask_cors import CORS, cross_origin
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from main import main
-import os
-import io
-from generateAssessment import generateAssessment
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
-@app.route("/")
-def starting():
-    return "Server is running"
-
-@app.route('/submit_quiz', methods=['POST'])
-@cross_origin(origin='*')
-def submit_quiz():
-    data = request.json  
-    print('Received quiz data for PDF generation:', data)
-
-    ans = main(data['tags'], data['numQuestions'], data['level'])
-    return jsonify({'message': 'Quiz submitted successfully'})
-
-@app.route('/startAssessment', methods=['POST'])
-@cross_origin(origin='*')
-def startAssessment():
-    data = request.json
-    print('Received quiz data to generate assessment:', data)
-
-    questionDetails = generateAssessment(data['tags'], data['numQuestions'], data['level'])
-    print(questionDetails)
-
-    return jsonify({'questions': questionDetails})
-   
-@app.route('/submit_assessment', methods=['POST'])
-def submit_assessment():
-    data = request.json
-    print('Received assessment data:', data)
-
-    total_marks = 0
-    marks_scored = 0
-
-    for key, values in data['feedback1'].items():
-        idx = int(key)
-        total_marks += 1
-        selected_answer = int(data['selectedOptions'][idx])
-        correct_answer = int(data['correctOptions'][key])
-
-        if selected_answer == correct_answer:
-            marks_scored += 1
-
-    return jsonify({'total_marks': total_marks, 'marks_scored': marks_scored})
-
-@app.route('/pdf-files/<filename>', methods=['GET'])
-def get_pdf(filename):
-    pdf_directory = './PDF/'  # Update with the actual path to your PDF files
-    return send_from_directory(pdf_directory, filename)
-
-@app.route('/pdf-files', methods=['GET'])
-def get_pdf_files():
-    print("Started fetching PDF files")
-    pdf_files = [file for file in os.listdir('./PDF') if file.endswith('.pdf')]
-    return jsonify(pdf_files)
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
 # from flask import Flask, render_template, request, jsonify, send_from_directory, send_file, url_for, redirect
