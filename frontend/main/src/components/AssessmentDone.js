04c22af01ef45ec642361ba159edca08515b0e04
Piyush <99687905+piuspk@users.noreply.github.com>
2025-01-29 12:27:41 +0530

    Update AssessmentDone.js

--- frontend/main/src/components/AssessmentDone.js
+++ frontend/main/src/components/AssessmentDone.js
@@ -173,7 +173,7 @@ const AssessmentDone = () => {
         };
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/save-assessment', {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/save-assessment`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
